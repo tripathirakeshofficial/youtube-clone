317b15fd58f78fdbde4ea6bb2c86acd7ba8c9f08
Rakesh Tripathi <rakeshtripathi408@gmail.com>
2023-01-12 16:39:21 +0530

    Videofeed Setup

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
 
-const Videos = () => <div>Videos</div>;
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map(
+        (item, idx) =>
+          (item.id.channelId || item.id.videoId) && (
+            <Box key={idx}>
+              {item.id.channelId && <ChannelCard channelDetail={item} />}
+              {item.id.videoId && <VideoCard video={item} />}
+            </Box>
+          )
+      )}
+    </Stack>
+  );
+};
 
 export default Videos;
