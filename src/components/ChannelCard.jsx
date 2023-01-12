53d63f8a9eb87a0cb364851cf8e351c0574b486f
Rakesh Tripathi <rakeshtripathi408@gmail.com>
2023-01-12 17:33:27 +0530

    Profilepage setup

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={{
       boxShadow: "none",
@@ -15,6 +15,7 @@ const ChannelCard = ({ channelDetail }) => (
       width: { xs: "356px", md: "315px" },
       height: "246px",
       margin: "auto",
+      marginTop,
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
