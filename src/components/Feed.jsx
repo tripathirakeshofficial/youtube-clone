71ba8a2df1e1356e4cef4a1c9b5810326d80926d
Rakesh Tripathi <rakeshtripathi408@gmail.com>
2023-01-12 13:38:34 +0530

    Feed setup

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Feed = () => {
-  return <div>Feed</div>;
-};
+import { Sidebar, Videos } from "./";
+
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box
+      sx={{
+        height: { sx: "auto", md: "92vh" },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      <Sidebar />
+      <Typography
+        className="copyright"
+        variant="body2"
+        sx={{ mt: 1.5, color: "#fff" }}
+      >
+        Copyright 2023 Rakesh Tripathi
+      </Typography>
+    </Box>
+
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        New <span style={{ color: "#fc1503" }}>videos</span>
+      </Typography>
+
+      <Videos videos={[]} />
+    </Box>
+  </Stack>
+);
 
 export default Feed;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Videos = () => <div>Videos</div>;
+
+export default Videos;
